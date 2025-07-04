e66c647918e2c94c86dac2f46e5cf42c2072d0c3
Maria Luiza <maluduartetorres@gmail.com>
2025-07-03 21:05:29 -0300

    :construction: Criação do componente categoria.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ function Navbar() {
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
       <h1 className="text-xl font-bold">Farmácia</h1>
       <div className="space-x-4">
-        <a href="/" className="hover:underline">Home</a>
-        <a href="/produtos" className="hover:underline">Produtos</a>
+        <a href="/categorias" className="hover:underline">Categorias</a>
+        <a href="/novacaRUtegoria" className="hover:underline">Cadastrar Categoria</a>
       </div>
     </nav>
   );
